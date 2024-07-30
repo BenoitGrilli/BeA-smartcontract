dcc3eb411ca531d4ad8c46e5e62e5ae3a54eb265
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2024-07-30 11:12:51 +0200

    indentation

--- contracts/BeA.sol
+++ contracts/BeA.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 contract BeA {
+    // Double mapping pour stocker les hash des utilisateurs
+    mapping(address => mapping(uint256 => bytes32)) private userHashes;
 
-// Double mapping pour stocker les hash des utilisateurs
-mapping(address => mapping(uint256 => bytes32)) private userHashes;
+    // Event pour notifier l'ajout d'un hash
+    event HashAdded(address indexed user, uint256 indexed index, bytes32 hash);
 
-// Event pour notifier l'ajout d'un hash
-event HashAdded(address indexed user, uint256 indexed index, bytes32 hash);
+    // Fonction pour ajouter un hash
+    function addHash(uint256 index, bytes32 hash) external {
+        userHashes[msg.sender][index] = hash;
+        emit HashAdded(msg.sender, index, hash);
+    }
 
-// Fonction pour ajouter un hash
-function addHash(uint256 index, bytes32 hash) external {
-    userHashes[msg.sender][index] = hash;
-    emit HashAdded(msg.sender, index, hash);
-}
-
-// Fonction pour vérifier si un hash existe
-function checkHash(address user, uint256 index) external view returns (bool) {
-    return userHashes[user][index] != bytes32(0);
-}
-
-
+    // Fonction pour vérifier si un hash existe
+    function checkHash(
+        address user,
+        uint256 index
+    ) external view returns (bool) {
+        return userHashes[user][index] != bytes32(0);
+    }
 }
