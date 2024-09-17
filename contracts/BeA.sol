5b64d0f6d57a9ff38065fd1a29ef7565290ce0ad
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2024-09-17 11:19:55 +0200

    updated

--- contracts/BeA.sol
+++ contracts/BeA.sol
@@ -4,14 +4,19 @@ pragma solidity ^0.8.0;
 contract BeA {
     // Double mapping pour stocker les hash des utilisateurs
     mapping(address => mapping(uint256 => bytes32)) private userHashes;
+    
+    // Mapping pour suivre l'index courant de chaque utilisateur
+    mapping(address => uint256) private userIndex;
 
     // Event pour notifier l'ajout d'un hash
     event HashAdded(address indexed user, uint256 indexed index, bytes32 hash);
 
     // Fonction pour ajouter un hash
-    function addHash(uint256 index, bytes32 hash) external {
-        userHashes[msg.sender][index] = hash;
-        emit HashAdded(msg.sender, index, hash);
+    function addHash(bytes32 hash) external {
+        uint256 currentIndex = userIndex[msg.sender];
+        userHashes[msg.sender][currentIndex] = hash;
+        emit HashAdded(msg.sender, currentIndex, hash);
+        userIndex[msg.sender] += 1;
     }
 
     // Fonction pour vérifier si un hash existe
@@ -20,5 +25,10 @@ contract BeA {
         uint256 index
     ) external view returns (bool) {
         return userHashes[user][index] != bytes32(0);
+    }
+    
+    // Fonction pour obtenir le prochain index pour un utilisateur
+    function getNextIndex(address user) external view returns (uint256) {
+        return userIndex[user];
     }
 }
